2b59b8196beef556aaa46ce5220c7b79e5623015
Nidhi <ndhpanchal1034@gmail.com>
2025-03-27 18:05:02 +0530

    final commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "../components/Weather.css";
+import "./Weather.css";
 
 const Weather = () => {
   const [city, setCity] = useState("");
